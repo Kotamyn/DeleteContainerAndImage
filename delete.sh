06bc531e8e29f614b719392807249534effca0cd
Kotamyn <67268519+Kotamyn@users.noreply.github.com>
2023-10-20 12:31:57 +0300

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo '↓ Enter name image ↓'
-read image
-
-CONTAINER=$(docker ps -aqf "ancestor=${image}")
-IMAGE=$(docker images | grep ${image} | awk '{ print $3}')
-
-docker stop $CONTAINER && docker rm $CONTAINER
-docker rmi $IMAGE
+if [ -n "$1" ]
+then
+      CONTAINER=$(docker ps -aqf "ancestor=${$1}")
+      IMAGE=$(docker images | grep ${$1} | awk '{ print $3}')
+      docker stop $CONTAINER && docker rm $CONTAINER
+      docker rmi $IMAGE
+else
+      echo "No parameters found. "
+fi
