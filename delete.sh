0e3f91b3b81f45745acbd2e4657ec7f69f8c7d61
Kotamyn <67268519+Kotamyn@users.noreply.github.com>
2023-10-20 12:36:02 +0300

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ -n "$1" ]
+input_images=$1
+if [ -n "$input_images" ]
 then
-      CONTAINER=$(docker ps -aqf "ancestor=${$1}")
-      IMAGE=$(docker images | grep ${$1} | awk '{ print $3}')
+      CONTAINER=$(docker ps -aqf "ancestor=${input_images}")
+      IMAGE=$(docker images | grep ${input_images} | awk '{ print $3}')
       docker stop $CONTAINER && docker rm $CONTAINER
       docker rmi $IMAGE
 else
